2120a314b5946f5702d3f8cb59121cc74609ba03
rlvizen <harel.vizen@mail.huji.ac.il>
2025-12-12 12:37:40 +0200

    12.12

--- src/ascii_art/AsciiArtAlgorithm.java
+++ src/ascii_art/AsciiArtAlgorithm.java
@@ -33,8 +33,14 @@ public class AsciiArtAlgorithm {
         char[][] asciiArt = new char[rows][cols];
         for (int i=0;i<rows;i++){
             for (int j=0;j<cols;j++){
-                asciiArt[i][j]=subImgCharMatcher.getCharByImageBrightness
-                        (ImageBrightnessCalculator.calculateBrightness(subImages[i][j]));
+                if (isReversed){
+                    asciiArt[i][j]=subImgCharMatcher.getCharByImageBrightness
+                            (1.0 - ImageBrightnessCalculator.calculateBrightness(subImages[i][j]));
+
+                } else {
+                    asciiArt[i][j]=subImgCharMatcher.getCharByImageBrightness
+                            (ImageBrightnessCalculator.calculateBrightness(subImages[i][j]));
+                }
             }
         }
         return asciiArt;
@@ -46,7 +52,7 @@ public class AsciiArtAlgorithm {
         char [] chars = {'0','1','2','3','4','5','6','7','8','9'};
         SubImgCharMatcher charMatcher = new SubImgCharMatcher(chars);
         Image image1 = new Image("cat.jpeg");
-        AsciiArtAlgorithm asciiArtAlgorithm = new AsciiArtAlgorithm(image1, 128, charMatcher, false);
+        AsciiArtAlgorithm asciiArtAlgorithm = new AsciiArtAlgorithm(image1, 128, charMatcher, true);
         char [][] asciiArt = asciiArtAlgorithm.run();
         consoleAsciiOutput.out(asciiArt);
         // Example usage
